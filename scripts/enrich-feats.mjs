1176ab80bfc1c82f3bf029e4dd7cb2f1c2bad17e
Joe <joe.ennis0@gmail.com>
2025-03-16 21:44:02 +0000

    Fix feat type bug including v3 compatability

--- scripts/enrich-feats.mjs
+++ scripts/enrich-feats.mjs
@@ -5,9 +5,24 @@ const FEAT_TYPES = new Map([
     ["origin", "Origin Feat"],
 ])
 
-export function getFeatType(ident) {
-    if (CONFIG.DND5E.featureTypes.feat.has(ident)) {
-        return CONFIG.DND5E.featureTypes.feat.get(ident)
+function getFeatTypeV3(ident) {
+    if (FEAT_TYPES.has(ident)) {
+        return FEAT_TYPES.get(ident)
     }
     return "Feat"
+}
+
+function getFeatTypeV4(ident) {
+    if (ident in CONFIG.DND5E.featureTypes.feat) {
+        return CONFIG.DND5E.featureTypes.feat[ident]
+    }
+    return "Feat"
+}
+
+export function getFeatType(ident) {
+    if (CONFIG.dndContentManager.systemV3) {
+        return getFeatTypeV3(ident)
+    } else {
+        return getFeatTypeV4(ident)
+    }
 }
